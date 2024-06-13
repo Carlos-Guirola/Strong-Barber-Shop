ce79266af6d4cd3a9c981a2a75d859471bba073d
Carlos-Guirola <158133600+Carlos-Guirola@users.noreply.github.com>
2024-06-13 11:32:52 -0600

    barberia

--- Strong/scrip.js
+++ Strong/scrip.js
@@ -3,21 +3,21 @@ let intervalId;
 
 function showSlides() {
   let slides = document.getElementsByClassName("image");
-  let firstIndex = slideIndex % slides.length;
-  let secondIndex = (slideIndex + 1) % slides.length;
-  let thirdIndex = (slideIndex + 2) % slides.length;
+  let firstIndex = (slideIndex - 1) % slides.length;
+  let secondIndex = slideIndex % slides.length;
+  let thirdIndex = (slideIndex + 1) % slides.length;
 
-  if (firstIndex === 0) firstIndex = slides.length;
-  if (secondIndex === 0) secondIndex = slides.length;
-  if (thirdIndex === 0) thirdIndex = slides.length;
+  if (firstIndex < 0) firstIndex += slides.length;
+  if (secondIndex < 0) secondIndex += slides.length;
+  if (thirdIndex < 0) thirdIndex += slides.length;
 
   for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  
-  slides[firstIndex - 1].style.display = "block";
-  slides[secondIndex - 1].style.display = "block";
-  slides[thirdIndex - 1].style.display = "block";
+
+  slides[firstIndex].style.display = "block";
+  slides[secondIndex].style.display = "block";
+  slides[thirdIndex].style.display = "block";
 }
 
 function startCarousel() {
@@ -36,4 +36,5 @@ function changeSlide(n) {
   showSlides();
 }
 
-showSlides(); 
+showSlides();
+
